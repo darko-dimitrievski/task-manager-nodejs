e414d3be3135fb1a34342516af74408b1da19916
Darko Dimitrievski <darko.dimitrievski@mk-hfd2k63437.home>
2025-05-27 02:20:13 +0200

    CORS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const connectDB = require ('./db/connect');
 const { response } = require('express');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 const tasks = require('./routes/tasks')
 require('dotenv').config();
 
